94529c36a20dd584b1fb01e1a078048cb5ff39a1
Danil Menkin <gundarixdan@gmail.com>
2024-08-02 10:03:29 +0400

    Encryption is working

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,13 @@
 use matrix::ByteMatrix;
 
-use crate::key::Key;
-
-mod encoding;
-mod key;
+pub mod block_cipher;
+pub mod key;
 mod matrix;
 mod sbox;
 
-#[allow(unused_variables)]
-pub fn encode(data: &[u8], key: [u8; 16]) -> Vec<u8> {
-    let keys = Key::new(key).expand();
-    panic!("");
-}
-
 const BLOCK_LEN_BIT: usize = 128;
 const BLOCK_LEN_BYTE: usize = BLOCK_LEN_BIT / 8;
-type Block = ByteMatrix<4, 4>;
+pub type Block = ByteMatrix<4, 4>;
 
 fn xor<const L: usize>(a: [u8; L], b: [u8; L]) -> [u8; L] {
     a.into_iter()
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,7 +1,7 @@
 use crate::xor;
 
-#[derive(Debug, PartialEq)]
-pub(crate) struct ByteMatrix<const M: usize, const N: usize> {
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct ByteMatrix<const M: usize, const N: usize> {
     inner: [[u8; N]; M],
     rows: usize,
     cols: usize,
@@ -17,7 +17,7 @@ impl<const M: usize, const N: usize> ByteMatrix<M, N> {
         }
     }
 
-    pub(crate) fn transpose(self) -> ByteMatrix<N, M> {
+    pub(crate) fn transposed(self) -> ByteMatrix<N, M> {
         let mut inner = [[0; M]; N];
         for i in 0..self.rows {
             for j in 0..self.cols {
@@ -31,20 +31,24 @@ impl<const M: usize, const N: usize> ByteMatrix<M, N> {
         }
     }
 
-    pub(crate) fn mul_mod<const P: usize>(self, other: [[u8; P]; N]) -> ByteMatrix<M, P> {
+    // Multiplication of two matrices. Client code must define their own multiplication and
+    // addition functions.
+    pub(crate) fn mul<const P: usize, MulFn, AddFn>(
+        self,
+        other: ByteMatrix<N, P>,
+        mul: MulFn,
+        add: AddFn,
+    ) -> ByteMatrix<M, P>
+    where
+        MulFn: Fn((u8, u8)) -> u8 + Copy,
+        AddFn: Fn(u8, u8) -> u8 + Copy,
+    {
         let mut result = [[0; P]; M];
-        if other.len() == 0 {
-            return result.into();
-        }
 
         for (i, row) in self.into_iter().enumerate() {
-            for j in 0..other[0].len() {
-                let col = other.map(|row| row[j]);
-                result[i][j] = row
-                    .into_iter()
-                    .zip(col.into_iter())
-                    .map(|(r, c)| (((r as u16) * (c as u16)) % 256u16) as u8)
-                    .fold(0, |acc, e| acc ^ e);
+            for j in 0..other.cols {
+                let col = other.inner.map(|row| row[j]);
+                result[i][j] = row.into_iter().zip(col.into_iter()).map(mul).fold(0, add);
             }
         }
         result.into()
@@ -56,6 +60,23 @@ impl<const M: usize, const N: usize> ByteMatrix<M, N> {
             inner[i] = xor(a, b)
         }
         inner.into()
+    }
+
+    pub(crate) fn map<F>(self, f: F) -> Self
+    where
+        F: Fn(u8) -> u8 + Copy,
+    {
+        self.inner.map(|row| row.map(f)).into()
+    }
+
+    pub(crate) fn rows_mut(&mut self) -> impl ExactSizeIterator<Item = &mut [u8; N]> {
+        self.inner.iter_mut()
+    }
+}
+
+impl<const M: usize, const N: usize> std::fmt::Display for ByteMatrix<M, N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", hex::encode::<Vec<_>>((*self).into()))
     }
 }
 
@@ -69,8 +90,8 @@ impl<const M: usize, const N: usize> From<[[u8; N]; M]> for ByteMatrix<M, N> {
     }
 }
 
-#[derive(Debug)]
-pub(crate) enum TryFromArrayError {
+#[derive(Debug, PartialEq)]
+pub enum TryFromArrayError {
     InvalidLen {
         len: usize,
         cols: usize,
@@ -83,31 +104,9 @@ impl std::fmt::Display for TryFromArrayError {
         match self {
             TryFromArrayError::InvalidLen { len, cols, rows } => write!(
                 f,
-                "Cannot convert array of len {len} into a matrix of size {rows}x{cols}"
+                "Cannot convert slice/array of len {len} into a matrix of size {rows}x{cols}"
             ),
         }
-    }
-}
-
-impl<const M: usize, const N: usize> TryFrom<&[u8]> for ByteMatrix<M, N> {
-    type Error = TryFromArrayError;
-
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        let mut matrix = ByteMatrix::new();
-        if value.len() != matrix.rows * matrix.cols {
-            return Err(TryFromArrayError::InvalidLen {
-                len: value.len(),
-                rows: matrix.rows,
-                cols: matrix.cols,
-            });
-        }
-
-        for i in 0..matrix.rows {
-            for j in 0..matrix.cols {
-                matrix.inner[i][j] = value[i * matrix.cols + j]
-            }
-        }
-        Ok(matrix)
     }
 }
 
@@ -133,6 +132,34 @@ impl<const M: usize, const N: usize, const S: usize> TryFrom<[u8; S]> for ByteMa
     }
 }
 
+impl<const M: usize, const N: usize> TryFrom<&[u8]> for ByteMatrix<M, N> {
+    type Error = TryFromArrayError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let mut matrix = ByteMatrix::new();
+        if value.len() != matrix.rows * matrix.cols {
+            return Err(TryFromArrayError::InvalidLen {
+                len: value.len(),
+                rows: matrix.rows,
+                cols: matrix.cols,
+            });
+        }
+
+        for i in 0..matrix.rows {
+            for j in 0..matrix.cols {
+                matrix.inner[i][j] = value[i * matrix.cols + j]
+            }
+        }
+        Ok(matrix)
+    }
+}
+
+impl<const M: usize, const N: usize> Into<Vec<u8>> for ByteMatrix<M, N> {
+    fn into(self) -> Vec<u8> {
+        self.inner.into_iter().flatten().collect()
+    }
+}
+
 impl<const M: usize, const N: usize> IntoIterator for ByteMatrix<M, N> {
     type Item = [u8; N];
     type IntoIter = std::array::IntoIter<Self::Item, M>;
@@ -144,59 +171,87 @@ impl<const M: usize, const N: usize> IntoIterator for ByteMatrix<M, N> {
 
 #[cfg(test)]
 mod tests {
+    use std::ops::BitXor;
+
     use super::*;
 
     #[test]
-    fn transpose() {
+    fn test_transpose() {
         let input = ByteMatrix::from([[1, 2, 3], [4, 5, 6]]);
         let result = [[1, 4], [2, 5], [3, 6]];
-        assert_eq!(input.transpose(), result.into());
+        assert_eq!(input.transposed(), result.into());
 
         let input = ByteMatrix::from([[2, 3, 1, 1], [1, 2, 3, 1], [1, 1, 2, 3], [3, 1, 1, 2]]);
         let result = [[2, 1, 1, 3], [3, 2, 1, 1], [1, 3, 2, 1], [1, 1, 3, 2]];
-        assert_eq!(input.transpose(), result.into());
+        assert_eq!(input.transposed(), result.into());
 
         let input = ByteMatrix::from([[], [], [], []]);
         let result: [[u8; 4]; 0] = [];
-        assert_eq!(input.transpose(), result.into());
+        assert_eq!(input.transposed(), result.into());
 
-        let input = ByteMatrix::from([]);
+        let input = ByteMatrix::try_from([] as [u8; 0]).unwrap();
         let result: [[u8; 0]; 0] = [];
-        assert_eq!(input.transpose(), result.into());
+        assert_eq!(input.transposed(), result.into());
     }
 
     #[test]
-    fn mul_mod() {
+    fn test_mul_mod() {
+        let mul_mod = |(a, b): (u8, u8)| -> u8 {
+            let res = a as u16 * b as u16;
+            (res % 0x100) as u8
+        };
+
         let a = ByteMatrix::from([[1, 2, 3], [4, 5, 6]]);
         let b = [[7, 8], [9, 10], [11, 12]];
         let c = [[52, 56], [115, 90]];
-        assert_eq!(a.mul_mod(b.into()), c.into());
+        assert_eq!(a.mul(b.into(), mul_mod, BitXor::bitxor), c.into());
 
         let a = ByteMatrix::from([[2, 3, 1, 1], [1, 2, 3, 1], [1, 1, 2, 3], [3, 1, 1, 2]]);
         let b = [[32], [81], [21], [106]];
         let c = [[204], [215], [101], [240]];
-        assert_eq!(a.mul_mod(b.into()), c.into());
+        assert_eq!(a.mul(b.into(), mul_mod, BitXor::bitxor), c.into());
 
         let a = ByteMatrix::from([[2, 3, 1, 1], [1, 2, 3, 1], [1, 1, 2, 3], [3, 1, 1, 2]]);
         let b = [[], [], [], []];
         let c = [[], [], [], []];
-        assert_eq!(a.mul_mod(b.into()), c.into());
+        assert_eq!(a.mul(b.into(), mul_mod, BitXor::bitxor), c.into());
 
         let a = ByteMatrix::from([[], [], [], []]);
-        let b = [];
+        let b: [u8; 0] = [];
         let c = [[], [], [], []];
-        assert_eq!(a.mul_mod(b.into()), c.into());
+        assert_eq!(
+            a.mul(b.try_into().unwrap(), mul_mod, BitXor::bitxor),
+            c.into()
+        );
     }
 
     #[test]
-    fn try_from() {
+    fn test_from() {
         assert_eq!(
-            ByteMatrix::<2, 3>::try_from([1, 2, 3, 4, 5, 6]).unwrap(),
-            ByteMatrix {
+            ByteMatrix::<2, 3>::try_from([1, 2, 3, 4, 5, 6]),
+            Ok(ByteMatrix {
                 inner: [[1, 2, 3], [4, 5, 6]],
                 rows: 2,
                 cols: 3
-            }
+            }),
+        );
+
+        assert_eq!(
+            ByteMatrix::<2, 3>::try_from([1, 2, 3, 4, 5]),
+            Err(TryFromArrayError::InvalidLen {
+                len: 5,
+                rows: 2,
+                cols: 3
+            }),
+        );
+
+        assert_eq!(
+            ByteMatrix::<2, 3>::try_from([1, 2, 3, 4, 5, 6, 7]),
+            Err(TryFromArrayError::InvalidLen {
+                len: 7,
+                rows: 2,
+                cols: 3
+            }),
         );
     }
 }
